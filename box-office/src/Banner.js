57a63ab3db779182a5b7e8f7ed9a8f34a075d7e7
sabhareeshkandikonda <sabhareeshkandikonda@partanalytics.com>
2021-10-04 01:24:01 +0530

    Fixed the styling of nav bar and top rated movies row

--- box-office/src/Banner.js
+++ box-office/src/Banner.js
@@ -47,7 +47,7 @@ function Banner() {
           {truncate(movie?.overview, 150)}
         </h1>
       </div>
-      <div className="banner__fadeBottom" />
+      <div className="banner--fadeBottom" />
     </header>
   );
 }

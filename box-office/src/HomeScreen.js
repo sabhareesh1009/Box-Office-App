7cd29b63a57b1255daec2299eb6fd803a7d6f325
sabhareeshkandikonda <sabhareeshkandikonda@partanalytics.com>
2021-10-04 00:34:26 +0530

    Implemented the movies rows and added firebase to first deploy

--- box-office/src/HomeScreen.js
+++ box-office/src/HomeScreen.js
@@ -12,7 +12,11 @@ function HomeScreen() {
 
       <Banner />
 
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchTrending} />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchTrending}
+        isLargeRow={true}
+      />
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
       <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
       <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />

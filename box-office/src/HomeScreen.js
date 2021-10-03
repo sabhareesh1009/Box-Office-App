95c650d24eddb8f82466c74a65038b0bec9a440f
sabhareeshkandikonda <sabhareeshkandikonda@partanalytics.com>
2021-10-03 19:37:09 +0530

    Added the home screen with Banner and nav bar

---
+++ box-office/src/HomeScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Banner from "./Banner";
+import "./HomeScreen.css";
+import Nav from "./Nav";
+
+function HomeScreen() {
+  return (
+    <div className="homeScreen">
+      <Nav />
+
+      <Banner />
+    </div>
+  );
+}
+
+export default HomeScreen;

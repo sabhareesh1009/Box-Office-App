aa1c6097e87125b46802c7bf6f39e6fbf16ae60e
sabhareeshkandikonda <sabhareeshkandikonda@partanalytics.com>
2021-10-03 19:35:59 +0530

    Initial launch of Box  office App

---
+++ box-office/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBKDzSLLsmPPlB-Gba9WZ-phhcrQc4DMTE",
+    authDomain: "box-office-5e075.firebaseapp.com",
+    projectId: "box-office-5e075",
+    storageBucket: "box-office-5e075.appspot.com",
+    messagingSenderId: "415119328087",
+    appId: "1:415119328087:web:f5fa3af78799662cea3412"
+  };

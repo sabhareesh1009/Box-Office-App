13b78b6287ed1a3682f19bd3a7291ec80639d058
sabhareesh1009 <sabhareeshkandikonda1109@gmail.com>
2021-10-06 02:55:25 +0530

    Added the singup form in login screen

--- box-office/src/screens/SignUpScreen.js
+++ box-office/src/screens/SignUpScreen.js
@@ -2,7 +2,21 @@ import React from "react";
 import "./SignUpScreen.css";
 
 function SignUpScreen() {
-  return <div></div>;
+  return (
+    <div className="signupScreen">
+      <form>
+        <h1>Sign In</h1>
+        <input placeholder="Email" type="email" />
+        <input placeholder="Password" type="password" />
+        <button type="submit">Sign In</button>
+
+        <h4>
+          <span className="signupScreen_gray">New to Netflix? </span>
+          <span className="signupScreen_link">Sign Up now.</span>
+        </h4>
+      </form>
+    </div>
+  );
 }
 
 export default SignUpScreen;
